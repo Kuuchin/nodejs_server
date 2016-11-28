5cc40979bd38f3b3b37685d6651f64a34ad42144
Dat Florentin <floren_d@epita.fr>
2016-11-28 15:23:00 +0100

    Fix path todo_utils

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var todo_utils = require('todo_utils');
+var todo_utils = require('../todo_utils');
 
 describe('#Error : Todo is null', function () {
    it('todo_utils._getAll should throw an error : todo is null', function () {
